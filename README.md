f410b48e866f43e547a323b34b26c896f7fb98bf
Lance McCarthy <lance.mccarthy@telerik.com>
2018-07-16 21:03:54 -0400

    update readme with screenshots.
    Added screenshots for RangeSelection demo app. Also, fixed whitespace issues on MainPage.xaml.cs

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # CustomXamarinDemos
+
+## RangeSelectionTest
+
+A demo that shows you how to use Xamarin Effects to access the native features of the UI for Xamarin RadCalendar and enable Range Selection on the native calendar control.
+
+### UWP
+![UWP](https://user-images.githubusercontent.com/3520532/42790664-515eb808-893a-11e8-8aed-a5ef529aa329.png)
+
+### iOS
+![iOS](https://user-images.githubusercontent.com/3520532/42790791-de7c9908-893a-11e8-86ed-73cfef765c3c.png)
+
+### Android
+![Android](https://user-images.githubusercontent.com/3520532/42790912-6788c190-893b-11e8-8e39-cf550eaafdb9.png)
--- RangeSelectionTest/RangeSelectionTest/Portable/MainPage.xaml.cs
+++ RangeSelectionTest/RangeSelectionTest/Portable/MainPage.xaml.cs
@@ -4,23 +4,24 @@ using Xamarin.Forms;
 namespace RangeSelectionTest.Portable
 {
     public partial class MainPage : ContentPage
-	{
-	    private int incrementer = 1;
-		public MainPage()
-		{
-			InitializeComponent();
+    {
+        private int incrementer = 1;
 
-		    SelectionEffect.StartDate = DateTime.Today.AddDays(-3);
-		    SelectionEffect.EndDate = DateTime.Today;
-		}
+        public MainPage()
+        {
+            InitializeComponent();
 
-	    private void Button_OnClicked(object sender, EventArgs e)
-	    {
-	        incrementer++;
+            SelectionEffect.StartDate = DateTime.Today.AddDays(-3);
+            SelectionEffect.EndDate = DateTime.Today;
+        }
 
-	        SelectionEffect.StartDate = DateTime.Today.AddDays(incrementer - 3);
-	        SelectionEffect.EndDate = DateTime.Today.AddDays(incrementer);
-            
-	    }
-	}
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            incrementer++;
+
+            SelectionEffect.StartDate = DateTime.Today.AddDays(incrementer - 3);
+            SelectionEffect.EndDate = DateTime.Today.AddDays(incrementer);
+
+        }
+    }
 }
