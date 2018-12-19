9cea1b962ef8a489c7c885cd4bea878199bd330c
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2018-12-19 17:46:02 -0500

    Added XamlC to DrawerDismissEffect demo

--- DrawerDismissEffect/DrawerDismissEffect/Portable/App.xaml.cs
+++ DrawerDismissEffect/DrawerDismissEffect/Portable/App.xaml.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
+// Update: Added XamlC to prevent iOS Linker from stripping out referenced assembly code
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace DrawerDismissEffect.Portable
 {
     public partial class App : Application
