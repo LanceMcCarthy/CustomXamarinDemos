f4045526f573a694049ac808ef964e9f83305308
Lance McCarthy <lance.mccarthy@progress.com>
2019-03-29 15:17:31 -0400

    RenderImage - Finished upgrades (Android cropping confirmed working)
    Remoevd unused Xamarin.Essentials and tested on platforms.

--- RenderImage/RenderImage/Android/MainActivity.cs
+++ RenderImage/RenderImage/Android/MainActivity.cs
@@ -25,18 +25,10 @@ namespace RenderImage.Android
             DependencyService.Register<RenderService>();
 
             // ********************************* //
-
-            Xamarin.Essentials.Platform.Init(this, bundle);
+            
             CrossCurrentActivity.Current.Init(this, bundle);
 
             LoadApplication(new Portable.App());
-        }
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
