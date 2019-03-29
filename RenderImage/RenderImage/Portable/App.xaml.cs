8b8f49b678855bbfbf635cff6f5386aa7aeb1ab2
Lance McCarthy <lance.mccarthy@progress.com>
2019-03-29 18:37:11 -0400

    RenderImage - Major Update: Added proportional crop with demo page
    The MainPage will redner the content and send it to a details page. ProportionalCrop lets you pass a percentage value proportional to the full resolution size.

--- RenderImage/RenderImage/Portable/App.xaml.cs
+++ RenderImage/RenderImage/Portable/App.xaml.cs
@@ -8,7 +8,7 @@ namespace RenderImage.Portable
         {
             InitializeComponent();
             
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
---
+++ RenderImage/RenderImage/Portable/ResultsPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace RenderImage.Portable
+{
+    public partial class ResultsPage : ContentPage
+    {
+        private byte[] _imageBytes;
+        
+        public ResultsPage (byte[] imageBytes)
+		{
+			InitializeComponent();
+
+            _imageBytes = imageBytes;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            try
+            {
+                BusyIndicator.IsBusy = true;
+
+                image.Source = ImageSource.FromStream(() => new MemoryStream(_imageBytes));
+
+                _imageBytes = null;
+            }
+            catch(Exception ex)
+            {
+                ResultsLabel.Text = $"Error: {ex.Message}";
+            }
+            finally
+            {
+                BusyIndicator.IsBusy = false;
+            }
+        }
+    }
+}
