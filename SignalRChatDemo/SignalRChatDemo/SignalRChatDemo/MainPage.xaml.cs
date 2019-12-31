f13ae9625ca351801515ac8869843f26806e0fb2
Lance McCarthy <lance.mccarthy@progress.com>
2019-12-31 16:08:34 -0500

    Added Real Time Typing Indicators via SignalR

--- SignalRChatDemo/SignalRChatDemo/SignalRChatDemo/MainPage.xaml.cs
+++ SignalRChatDemo/SignalRChatDemo/SignalRChatDemo/MainPage.xaml.cs
@@ -12,7 +12,7 @@ namespace SignalRChatDemo
     public partial class MainPage : ContentPage
     {
         private ChatService service;
-        private Author me = new Author {Name = "Xamarin.Forms User"};
+        private Author me = new Author { Name = "Xamarin.Forms User" };
         private ObservableCollection<Author> participants = new ObservableCollection<Author>();
 
         public MainPage()
@@ -24,6 +24,7 @@ namespace SignalRChatDemo
             ChatControl.Author = me;
 
             ((INotifyCollectionChanged)ChatControl.Items).CollectionChanged += ChatItems_CollectionChanged;
+            MyTypingIndicator.Authors.CollectionChanged += TypingIndicatorAuthors_CollectionChanged;
         }
 
         protected override async void OnAppearing()
@@ -37,6 +38,7 @@ namespace SignalRChatDemo
             // Create the SignalR service class
             service = new ChatService("https://uiforxamarinchatserver.azurewebsites.net/ChatHub");
             service.OnMessageReceived += ServiceOnMessageReceived;
+            service.OnTypersUpdated += Service_OnTypersUpdated;
 
             // Start the service to open the connection
             await service.StartAsync();
@@ -44,6 +46,48 @@ namespace SignalRChatDemo
             // Hide the busy indicator
             BusyIndicator.IsBusy = false;
             BusyIndicator.IsVisible = false;
+        }
+
+        private void Service_OnTypersUpdated(string username, bool isTyping)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                // no need to check if the current user was changed
+                if (username == me.Name)
+                {
+                    return;
+                }
+
+                // Check if the author is already in the local list of chart participants
+                Author author = participants.FirstOrDefault(a => a.Name == username);
+
+                // If they haven't add them now and add them to the collection
+                if (author == null)
+                {
+                    author = new Author
+                    {
+                        Name = username
+                    };
+
+                    if (!participants.Contains(author))
+                        participants.Add(author);
+                }
+
+                if (isTyping) // add author to typers list
+                {
+                    if (!MyTypingIndicator.Authors.Contains(author))
+                    {
+                        MyTypingIndicator.Authors.Add(author);
+                    }
+                }
+                else // remove author if they've stopped typing
+                {
+                    if (MyTypingIndicator.Authors.Contains(author))
+                    {
+                        MyTypingIndicator.Authors.Remove(author);
+                    }
+                }
+            });
         }
 
         private void ServiceOnMessageReceived(string username, string message)
@@ -67,7 +111,7 @@ namespace SignalRChatDemo
                         Name = username
                     };
 
-                    if(!participants.Contains(author))
+                    if (!participants.Contains(author))
                         participants.Add(author);
                 }
 
@@ -97,6 +141,39 @@ namespace SignalRChatDemo
                     var messageToSend = chatMessage.Text;
 
                     await service.SendMessageAsync(authorName, messageToSend);
+                }
+            }
+        }
+
+        private async void TypingIndicatorAuthors_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null && e.NewItems.Contains(me))
+            {
+                await service.SendTyperAsync(me.Name, true);
+            }
+
+            if (e.OldItems != null && e.OldItems.Contains(me))
+            {
+                await service.SendTyperAsync(me.Name, false);
+            }
+        }
+
+        private void RadEntry_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.NewTextValue))
+            {
+                // If the text is not empty add the user to the current typers list
+                if (!MyTypingIndicator.Authors.Contains(me))
+                {
+                    MyTypingIndicator.Authors.Add(me);
+                }
+            }
+            else
+            {
+                // USer has sent message because text is empty, remove them from the typers
+                if (MyTypingIndicator.Authors.Contains(me))
+                {
+                    MyTypingIndicator.Authors.Remove(me);
                 }
             }
         }
