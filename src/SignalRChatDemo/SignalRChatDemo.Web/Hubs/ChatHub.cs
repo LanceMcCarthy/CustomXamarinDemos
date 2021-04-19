e052f486f2bd8814154e576eec685eb1e7183d14
Lance McCarthy <lance@dvlup.com>
2021-04-19 18:44:16 -0400

    Add new Web server project

---
+++ src/SignalRChatDemo/SignalRChatDemo.Web/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatDemo.Web.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendTyper(string user, bool isTyping)
+        {
+            await Clients.All.SendAsync("ReceiveTyper", user, isTyping);
+        }
+    }
+}
