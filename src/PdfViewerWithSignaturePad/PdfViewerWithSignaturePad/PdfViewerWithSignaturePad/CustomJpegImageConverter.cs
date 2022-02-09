54b55db863e3c6067cea304541015d2c0bc1c049
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-02-09 17:07:34 -0500

    Add PNG support to SignaturePad and PdfProcessing Demo

---
+++ src/PdfViewerWithSignaturePad/PdfViewerWithSignaturePad/PdfViewerWithSignaturePad/CustomJpegImageConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
+using Telerik.Windows.Documents.Extensibility;
+using Telerik.Windows.Documents.Fixed.FormatProviders.Pdf.Export;
+
+namespace PdfViewerWithSignaturePad
+{
+    public class CustomJpegImageConverter : JpegImageConverterBase
+    {
+        public override bool TryConvertToJpegImageData(byte[] imageData, ImageQuality imageQuality, out byte[] jpegImageData)
+        {
+            var imageSharpImageFormats = new[] { "jpeg", "bmp", "png", "gif" };
+
+            if (this.TryGetImageFormat(imageData, out var imageFormat) && imageSharpImageFormats.Contains(imageFormat.ToLower()))
+            {
+                // Install the SixLabors.ImageSharp to the class library, see https://docs.sixlabors.com/articles/imagesharp/index.html
+                using (SixLabors.ImageSharp.Image imageSharp = SixLabors.ImageSharp.Image.Load(imageData))
+                {
+                    imageSharp.Mutate(x => x.BackgroundColor(SixLabors.ImageSharp.Color.White));
+
+                    using (var ms = new MemoryStream())
+                    {
+                        SixLabors.ImageSharp.ImageExtensions.SaveAsJpeg(imageSharp, ms, new JpegEncoder
+                        {
+                            Quality = (int)imageQuality,
+                        });
+
+                        jpegImageData = ms.ToArray();
+                    }
+                }
+
+                return true;
+            }
+
+            jpegImageData = null;
+
+            return false;
+        }
+    }
+}
--- src/PdfViewerWithSignaturePad/PdfViewerWithSignaturePad/PdfViewerWithSignaturePad/MainPage.xaml.cs
+++ src/PdfViewerWithSignaturePad/PdfViewerWithSignaturePad/PdfViewerWithSignaturePad/MainPage.xaml.cs
@@ -1,8 +1,15 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Telerik.Windows.Documents.Extensibility;
 using Telerik.Windows.Documents.Fixed.FormatProviders.Pdf;
+using Telerik.Windows.Documents.Fixed.FormatProviders.Pdf.Export;
 using Telerik.Windows.Documents.Fixed.Model;
+using Telerik.Windows.Documents.Fixed.Model.Annotations;
+using Telerik.Windows.Documents.Fixed.Model.Editing;
 using Telerik.XamarinForms.Input;
 using Telerik.XamarinForms.PdfViewer;
 using Xamarin.Forms;
@@ -12,6 +19,8 @@ namespace PdfViewerWithSignaturePad
     public partial class MainPage : ContentPage
     {
         private RadFixedDocument originalDocument;
+        private readonly List<string> formats = new List<string> { "JPEG", "PNG" };
+        private ImageFormat selectedFormat = ImageFormat.Jpeg;
 
         public MainPage()
         {
@@ -22,23 +31,39 @@ namespace PdfViewerWithSignaturePad
         {
             base.OnAppearing();
             
-            originalDocument = HelperMethods.ImportPdfFileToFixedDocument("Original.pdf");
+            FormatComboBox.ItemsSource = formats;
+            FormatComboBox.SelectedItem = formats[0];
 
+            originalDocument = ImportPdfFileToFixedDocument("Original.pdf");
             PdfViewer1.Source = new FixedDocumentSource(originalDocument);
+        }
+
+        private void FormatComboBox_OnSelectionChanged(object sender, ComboBoxSelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Any() && e.AddedItems.First() is string selectedValue)
+            {
+                selectedFormat = selectedValue == "JPEG" ? ImageFormat.Jpeg : ImageFormat.Png;
+            }
+        }
+
+        private void ClearButton_OnClicked(object sender, EventArgs e)
+        {
+            SignaturePad1.ClearCommand.Execute(null);
         }
 
         private async void SignAndSaveButton_OnClicked(object sender, EventArgs e)
         {
+            // Release any possible reference clash to the current document
+            PdfViewer1.Source = null;
+
             var localFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
             
             // Option 1 - using filepath for signature image source
-            var signatureImageFilePath = Path.Combine(localFolder, "Signature.jpg");
-            await HelperMethods.SaveSignatureImageAsync(SignaturePad1, ImageFormat.Jpeg, signatureImageFilePath);
-            var signedDocument = HelperMethods.AddSignatureToDocument(originalDocument, signatureImageFilePath, ImageFormat.Jpeg);
+            var fileExt = selectedFormat == ImageFormat.Jpeg ? "jpg" : "png";
+            var signatureImageFilePath = Path.Combine(localFolder, $"Signature.{fileExt}");
 
-            // Option 2 - using byte[] for signature image source
-            // var imgBytes = await HelperMethods.GetSignatureImageAsync(SignaturePad1, ImageFormat.Jpeg);
-            // var signedDocument = HelperMethods.AddSignatureToDocument(originalDocument, imgBytes, ImageFormat.Jpeg);
+            await SaveSignatureImageAsync(SignaturePad1, selectedFormat, signatureImageFilePath);
+            var signedDocument = AddSignatureToDocument(originalDocument, signatureImageFilePath, selectedFormat);
             
             // Save the updated document
             if (signedDocument != null)
@@ -60,6 +85,139 @@ namespace PdfViewerWithSignaturePad
             {
                 await DisplayAlert("No Signature Field", "There was no signature field detected in the document, signing incomplete.", "OK");
             }
+        }
+
+        public static RadFixedDocument ImportPdfFileToFixedDocument(string pathOrResourceName, bool isEmbeddedResource = true)
+        {
+            if (isEmbeddedResource)
+            {
+                Assembly assembly = typeof(MainPage).Assembly;
+                string fileName = assembly.GetManifestResourceNames().FirstOrDefault(n => n.Contains(pathOrResourceName));
+
+                using (var stream = assembly.GetManifestResourceStream(fileName))
+                {
+                    var provider = new Telerik.Windows.Documents.Fixed.FormatProviders.Pdf.PdfFormatProvider();
+                    return provider.Import(stream);
+                }
+            }
+            else
+            {
+                using (var stream = File.OpenRead(pathOrResourceName))
+                {
+                    var provider = new Telerik.Windows.Documents.Fixed.FormatProviders.Pdf.PdfFormatProvider();
+                    return provider.Import(stream);
+                }
+            }
+        }
+
+        public static async Task SaveSignatureImageAsync(RadSignaturePad pad, ImageFormat imgFormat, string filePath)
+        {
+            // Create a MemoryStream to load the image into
+            using (var imgStream = new MemoryStream())
+            {
+                // Grab the user's signature image from the SignaturePad
+                var saveSettings = new SaveImageSettings()
+                {
+                    ImageFormat = imgFormat,
+                    ScaleFactor = 0.7,
+                    ImageQuality = 1,
+                    BackgroundColor = Color.White,
+                    StrokeColor = Color.DarkBlue,
+                    StrokeThickness = 5
+                };
+
+                // Save the written signature as an image
+                await pad.SaveImageAsync(imgStream, saveSettings);
+
+                if (imgStream.CanSeek)
+                {
+                    imgStream.Seek(0, SeekOrigin.Begin);
+                }
+                else
+                {
+                    imgStream.Position = 0;
+                }
+
+                var imgBytes = imgStream.ToArray();
+
+                File.WriteAllBytes(filePath, imgBytes);
+            }
+        }
+
+        public static RadFixedDocument AddSignatureToDocument(RadFixedDocument originalDocument, string imageFilePath, ImageFormat imgFormat)
+        {
+            // We'll use a clone of the original document to perform editing operations with (just in case something goes wrong)
+            RadFixedDocument workingDocument = originalDocument.Clone();
+
+            var formFieldsCount = workingDocument.AcroForm.FormFields.Count;
+
+            if (formFieldsCount == 0)
+            {
+                return null;
+            }
+
+            foreach (var formField in workingDocument.AcroForm.FormFields)
+            {
+                foreach (var widget in formField.Widgets)
+                {
+                    if (widget.WidgetContentType == WidgetContentType.SignatureContent)
+                    {
+                        // We've discovered a SignatureWidget in the document.
+                        var signatureWidget = widget;
+
+                        // WARNING - this demo has the signature on the first page, if yours is on a different page, you'll need to update this
+                        var currentPage = workingDocument.Pages.First();
+
+                        // An easy way to edit the document is to use a FixedContentEditor. See https://docs.telerik.com/devtools/document-processing/libraries/radpdfprocessing/editing/fixedcontenteditor
+                        FixedContentEditor editor = new FixedContentEditor(currentPage);
+
+                        // Move the Editor to the same location on the page that the SignatureWidget was. See https://docs.telerik.com/devtools/document-processing/libraries/radpdfprocessing/concepts/position
+                        editor.Position.Translate(signatureWidget.Rect.X, signatureWidget.Rect.Y);
+
+                        // Get the dimensions of the widget to use for the size of the image
+                        var imageHeight = Convert.ToInt32(signatureWidget.Rect.Height);
+                        var imageWidth = Convert.ToInt32(signatureWidget.Rect.Width);
+
+                        // Load the image file into a byte[]
+                        var fileImageBytes = File.ReadAllBytes(imageFilePath);
+
+                        Telerik.Windows.Documents.Fixed.Model.Resources.ImageSource imageSource = null;
+
+                        // Because we have a JPEG image already, we can pass it to the new ImageSource object
+                        if (imgFormat == ImageFormat.Jpeg)
+                        {
+                            imageSource = new Telerik.Windows.Documents.Fixed.Model.Resources.ImageSource(new MemoryStream(fileImageBytes));
+                        }
+
+                        // However, if we have a PNG image, we need to convert it to JPEG first.
+                        if (imgFormat == ImageFormat.Png)
+                        {
+                            // IMPORTANT: You need to define a PNG to JPEG converter in a .NET Standard 2.0 project
+                            // See https://docs.telerik.com/devtools/document-processing/libraries/radpdfprocessing/cross-platform#limitations-in-net-standard
+                            FixedExtensibilityManager.JpegImageConverter = new CustomJpegImageConverter();
+
+                            // onvert the PNG data to JPEG data
+                            FixedExtensibilityManager.JpegImageConverter.TryConvertToJpegImageData(fileImageBytes, ImageQuality.Low, out byte[] convertedImageBytes);
+
+                            // Create the ImageSource object
+                            imageSource = new Telerik.Windows.Documents.Fixed.Model.Resources.ImageSource(new MemoryStream(convertedImageBytes));
+                        }
+
+                        if (imageSource == null)
+                            return null;
+
+                        // Draw the image on the document. See https://docs.telerik.com/devtools/document-processing/libraries/radpdfprocessing/editing/fixedcontenteditor#inserting-image
+                        editor.DrawImage(imageSource, imageWidth, imageHeight);
+
+                        // Finally, remove the SignatureWidget
+                        currentPage.Annotations.Remove(signatureWidget);
+
+                        return workingDocument;
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
